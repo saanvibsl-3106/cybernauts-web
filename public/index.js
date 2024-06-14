e8363cb60a5e77864be38a21c7dcf4249b6ea016
saanvibsl-3106 <146667228+saanvibsl-3106@users.noreply.github.com>
2024-06-14 19:39:25 +0530

    new

---
+++ public/index.js
@@ -0,0 +1,20 @@
+import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
+
+const root = document.getElementById("app");
+const options = {
+    el: root,
+    shaderPoints: 16,
+    curvePoints:20,
+    curveLerp: 1,
+    radius1: 1,
+    radius2: 5,
+    velocityTreshold: 10,
+    sleepRadiusY: 100,
+    sleepRadiusX: 100,
+    sleepTimeCoefX: 0.00025,
+    sleepTimeCoefY: 0.001,
+
+};
+
+
+neonCursor(options)

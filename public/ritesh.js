9590b62e125b4945db10d0c8ff1834864669b019
ritesh <riteshsaini1601@gmail.com>
2024-06-27 16:43:31 +0530

    I made my part

--- public/ritesh.js
+++ public/ritesh.js
@@ -37,10 +37,6 @@ document.getElementById("Explore_Button").addEventListener("click", function() {
 
 
 
-
-
-
-
 /*----------------------------------------------------------------------------------------------------------------------------- */
 /* Animation of Amazing-Crazy-Cool */
 
@@ -73,3 +69,117 @@ amazingCrazyCool();
 
 /* Animation of Amazing-Crazy-Cool */
 /*----------------------------------------------------------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+// // Responsive text sizefunction adjustFontSize() {
+    const width = window.innerWidth;
+    let scaleFactor;
+
+    console.log(width);
+
+    if (width <= 755) {
+      scaleFactor = '0.24';
+  } else if (width <= 782) {
+      scaleFactor = '0.29';
+  } 
+  else if (width <= 835) {
+    scaleFactor = '0.34';
+}
+else if (width <= 885) {
+  scaleFactor = '0.39';
+} 
+  else if (width <= 875) {
+    scaleFactor = '0.44';
+}else if (width <= 912) {
+  scaleFactor = '0.45';
+}
+else if (width <= 945) {
+  scaleFactor = '0.49';
+}
+else if (width <= 982) {
+  scaleFactor = '0.54';
+}
+else if (width <= 992) {
+      scaleFactor = '0.59';
+  }
+  else if (width <= 1149) {
+    scaleFactor = '0.64';
+  }
+  else if (width <= 1070) {
+    scaleFactor = '0.69';
+  }
+  else if (width <= 1130) {
+    scaleFactor = '0.74';
+  } else if (width <= 1200) {
+      scaleFactor = '0.79';
+  }
+  else if (width >=2500){
+     scaleFactor='1.49'
+  } else {
+      scaleFactor = '1';
+  }
+    const elements = [
+        ...document.querySelectorAll('.font-size1'),
+        ...document.querySelectorAll('.font-size2'),
+        ...document.querySelectorAll('.font-size3'),
+        ...document.querySelectorAll('.font-size4'),
+        ...document.querySelectorAll('.text')
+    ];
+
+    elements.forEach(element => {
+        const baseFontSize = parseFloat(getComputedStyle(element).fontSize);
+        element.style.fontSize = `${baseFontSize * scaleFactor}px`;
+    });
+
+    console.log(scaleFactor);
+
+
+function adjustImageSize() {
+    const images = document.querySelectorAll('.responsive-image');
+    const width = window.innerWidth;
+    let imageWidth;
+
+    if (width <= 576) {
+        imageWidth = '50%';
+    } else if (width <= 768) {
+        imageWidth = '60%';
+    } else if (width <= 992) {
+        imageWidth = '70%';
+    } else if (width <= 1200) {
+        imageWidth = '80%';
+    } else {
+        imageWidth = '100%';
+    }
+
+    images.forEach(image => {
+        image.style.width = imageWidth;
+    });
+}
+
+function adjustSizes() {
+    adjustFontSize();
+    adjustImageSize();
+}
+
+window.addEventListener('load', adjustSizes);
+window.addEventListener('resize', adjustSizes);
+window.addEventListener('reload', adjustSizes);
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+//Flipping the image of faculty when hovered
+
+
+
+
